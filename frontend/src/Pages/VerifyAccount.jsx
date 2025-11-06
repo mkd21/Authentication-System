d440068f56f2c08b5842ac6128f08606f032f855
Mayank Deep <rishusinha525@gmail.com>
2025-11-07 02:17:35 +0530

    Added otp acceptance form

--- frontend/src/Pages/VerifyAccount.jsx
+++ frontend/src/Pages/VerifyAccount.jsx
@@ -1,9 +1,73 @@
 
+import { useState , useRef, useEffect } from "react";
+
 function VerifyAccount()
 {
+
+    const [formBox , updateBox] = useState(new Array(4).fill(""));
+  
+    const inputRefs = useRef([]);
+    
+
+    const handleChange = (index , e) =>{
+        
+        const updatedArr = [...formBox];
+        updatedArr[index] = e.target.value;
+        updateBox(updatedArr);
+
+        if(e.target.value && index < formBox.length-1)
+        {
+            inputRefs.current[index + 1].focus();
+        }
+
+        const otp = updatedArr.join("");
+
+        if(otp.length == formBox.length) console.log("serval call made with opt",otp);
+    }
+
+    useEffect( () => {
+
+        if(inputRefs.current[0]){
+            inputRefs.current[0].focus();
+        }
+
+    } , [] );
+
+    const handleClick = () => {}
+
+    const handleKeyDown = (e , index) => {
+        
+        if(e.key == "Backspace" && index > 0)
+        {
+            inputRefs.current[index - 1].focus();
+        }
+
+    }
+
     return (
         <>
-            <h1>Verify Account Page</h1>
+            
+            <form>
+                {
+                    formBox.map( (val , idx) => (
+                        <input 
+                            type="string" 
+                            inputMode="numeric"
+                            pattern="[0-9]*"
+                            key={idx} 
+                            value={val}
+                            maxLength={1}
+                            ref={ (e) => inputRefs.current[idx] = e }
+                            className="border-2 border-black mt-12 mx-3 w-14 ps-6 focus:outline-blue-300 "
+                            onChange={ (e) => handleChange(idx , e)}
+                            onClick={handleClick}
+                            onKeyUp={ (e) =>  handleKeyDown(e , idx)}
+                        />)
+                    )
+                }
+            </form>
+    
+
         </>
     );
 }
